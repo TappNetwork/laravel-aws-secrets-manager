1a3b98928a8e0e841e4e595c554b1921bb3ea905
Carlita Wiggan <info@cwiggan.com>
2020-07-27 17:41:38 -0400

    add aws key and access code

--- config/config.php
+++ config/config.php
@@ -12,6 +12,8 @@ return [
     */
 
     'region' => env('AWS_DEFAULT_REGION'),
+    'key' => env('AWS_ACCESS_KEY_ID'),
+    'secret' => env('AWS_SECRET_ACCESS_KEY'),
 
     /*
     |--------------------------------------------------------------------------
