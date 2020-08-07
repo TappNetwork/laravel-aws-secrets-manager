4132d3cf1a5e237b15e93a7d60e5a42ea0fd8f53
Carlita Wiggan <info@cwiggan.com>
2020-08-07 12:15:27 -0400

    remove aws keys

--- config/config.php
+++ config/config.php
@@ -12,8 +12,6 @@ return [
     */
 
     'region' => env('AWS_DEFAULT_REGION'),
-    'key' => env('AWS_ACCESS_KEY_ID'),
-    'secret' => env('AWS_SECRET_ACCESS_KEY'),
 
     /*
     |--------------------------------------------------------------------------
