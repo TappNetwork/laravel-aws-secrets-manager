3370235da07b1513de93d29a3f2ee58357735c56
Frederick Vanbrabant <frederick.vanbrabant@gmail.com>
2020-08-12 11:26:56 +0200

    Renamed the test

--- tests/SetupTest.php
+++ tests/SetupTest.php
@@ -5,7 +5,7 @@ namespace Tapp\LaravelAwsSecretsManager\Tests;
 use Orchestra\Testbench\TestCase;
 use Tapp\LaravelAwsSecretsManager\LaravelAwsSecretsManagerServiceProvider;
 
-class ExampleTest extends TestCase
+class SetupTest extends TestCase
 {
     protected function getPackageProviders($app)
     {
